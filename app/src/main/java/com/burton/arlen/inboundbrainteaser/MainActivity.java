f9b44c628dc00146f8b0120f27d4a6a37a11690b
arlen <burton.arlen.1990@gmail.com>
2017-06-28 15:34:17 -0700

    start short

--- app/src/main/java/com/burton/arlen/inboundbrainteaser/MainActivity.java
+++ app/src/main/java/com/burton/arlen/inboundbrainteaser/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.goButton)Button goButton;
     public int[] a = new int[]{4,5,6};
     private boolean condition;
+    private boolean conditionShort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +27,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void solveBrainTeaserLong(){
         for(int i = 0; i < a.length; i++){
             for (int x = 0; x < a.length; x++){
-                Log.d("return a[i]", String.valueOf(a[i]));
-                Log.d("return a[x]", String.valueOf(a[x]));
-                Log.d("return total", String.valueOf(a[i]+a[x]));
+//                Log.d("return a[i]", String.valueOf(a[i]));
+//                Log.d("return a[x]", String.valueOf(a[x]));
+//                Log.d("return total", String.valueOf(a[i]+a[x]));
                 if (a[i] + a[x] == 10){
                     condition = true;
-                    Log.d("condition met", String.valueOf(condition));
+//                    Log.d("condition met", String.valueOf(condition));
                 } else {
                     condition = false;
-                    Log.d("condition met", String.valueOf(condition));
+//                    Log.d("condition met", String.valueOf(condition));
                 }
             }
         }
     }
 
-    private boolean solveShort(){
+    private void solveShort(){
         Log.d("Short", "started");
-        return false;
+        int g = 0;
+        for (int w = 0; w < a.length; w++){
+                int b = a.length - 1;
+                int f = b - g;
+            if (a[w] + a[f] != 10){
+                g = g + 1;
+                conditionShort = false;
+                Log.d("return a[w]", String.valueOf(a[w]));
+                Log.d("return a[f]", String.valueOf(a[f]));
+                Log.d("return total", String.valueOf(a[w]+a[f]));
+                Log.d("condition met", String.valueOf(conditionShort));
+            } else {
+                if (w < 0){
+                    w--;
+                } else {
+                    w = 0;
+                }
+                conditionShort = true;
+                Log.d("return a[w]", String.valueOf(a[w]));
+                Log.d("return a[f]", String.valueOf(a[f]));
+                Log.d("return total", String.valueOf(a[w]+a[f]));
+                Log.d("condition met", String.valueOf(conditionShort));
+            }
+        }
     }
 
     @Override
