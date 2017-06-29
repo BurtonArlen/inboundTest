46ed2c1beb198ae68426b45cfe34e72828ff9ee0
arlen <burton.arlen.1990@gmail.com>
2017-06-28 20:58:00 -0700

    clean up validation

--- app/src/main/java/com/burton/arlen/inboundbrainteaser/MainActivity.java
+++ app/src/main/java/com/burton/arlen/inboundbrainteaser/MainActivity.java
@@ -16,74 +16,120 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.goButton)Button goButton;
-    public int[] a = new int[]{4,5,6};
+    public int[] a = new int[]{4,6,2,8,9,5,7,1,3};
     private Map<Integer, Integer> map = new HashMap<>(a.length);
+    private boolean conditionSmallData;
     private boolean condition;
-    private boolean conditionShort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
         goButton.setOnClickListener(this);
         createMap();
     }
 
-    private void solveBrainTeaserLong(){
+    private void solutionBigData(){
         for(int i = 0; i < a.length; i++){
             for (int x = 0; x < a.length; x++){
-//                Log.d("return a[i]", String.valueOf(a[i]));
-//                Log.d("return a[x]", String.valueOf(a[x]));
-//                Log.d("return total", String.valueOf(a[i]+a[x]));
                 if (a[i] + a[x] == 10){
                     condition = true;
-//                    Log.d("condition met", String.valueOf(condition));
                 } else {
                     condition = false;
-//                    Log.d("condition met", String.valueOf(condition));
                 }
             }
         }
     }
 
     private void createMap() {
-        for (Integer mapping : a)
-        {
+        for (Integer mapping : a){
             map.put(mapping, mapping);
-            Log.d("mapContents", String.valueOf(map));
         }
-        Log.d("mapContents", String.valueOf(map));
     }
 
-    private void solveShort(){
+    private void solutionSmallData(){
         for(int v = 0; v < map.size(); v++){
-            int int1;
-            int int2;
-            int1 = map.get(a[v]);
-            int2 = (int1 - 10)*-1;
+            int int1 = map.get(a[v]);
+            int int2 = (int1 - 10)*-1;
             if (map.containsValue(int2)){
-                Log.d("value of sum", "is equal to 10");
-                Log.d("return int1", String.valueOf(int1));
-                Log.d("return int2", String.valueOf(int2));
-                Log.d("return sum", String.valueOf(int1 + int2));
+                if (int1 + map.get(int2) == 10){
+                    Log.d("value of sum", "is equal to 10");
+                    Log.d("return int1", String.valueOf(int1));
+                    Log.d("return int2", String.valueOf(int2));
+                    Log.d("return sum", String.valueOf(int1 + int2));
+                    conditionSmallData = true;
+                }
             } else {
-                Log.d("value of sum", "is NOT equal to 10");
-                Log.d("return int1", String.valueOf(int1));
-                Log.d("return int2", String.valueOf(int2));
-                Log.d("return sum", String.valueOf(int1 + int2));
+                conditionSmallData = false;
             }
         }
     }
 
-
-
     @Override
     public void onClick(View v){
         if (v == goButton){
-            solveBrainTeaserLong();
-            solveShort();
+            solutionBigData();
+            solutionSmallData();
         }
     }
 }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        ButterKnife.bind(this);
+//        goButton.setOnClickListener(this);
+//        createMap();
+//    }
+//
+//    private void solveBrainTeaserLong(){
+//        for(int i = 0; i < a.length; i++){
+//            for (int x = 0; x < a.length; x++){
+////                Log.d("return a[i]", String.valueOf(a[i]));
+////                Log.d("return a[x]", String.valueOf(a[x]));
+////                Log.d("return total", String.valueOf(a[i]+a[x]));
+//                if (a[i] + a[x] == 10){
+//                    condition = true;
+////                    Log.d("condition met", String.valueOf(condition));
+//                } else {
+//                    condition = false;
+////                    Log.d("condition met", String.valueOf(condition));
+//                }
+//            }
+//        }
+//    }
+//
+//    private void createMap() {
+//        for (Integer mapping : a)
+//        {
+//            map.put(mapping, mapping);
+//            Log.d("mapContents", String.valueOf(map));
+//        }
+//        Log.d("mapContents", String.valueOf(map));
+//    }
+//
+//    private void solveShort(){
+//        for(int v = 0; v < map.size(); v++){
+//            int int1;
+//            int int2;
+//            int1 = map.get(a[v]);
+//            int2 = (int1 - 10)*-1;
+//            if (map.containsValue(int2)){
+//            } else {
+//            }
+//        }
+//    }
+//
+//
+//
+//    @Override
+//    public void onClick(View v){
+//        if (v == goButton){
+//            solveBrainTeaserLong();
+//            solveShort();
+//        }
+//    }
+//}
